9e6bc8f28ee33e95e9bd1e96440a7bc899a4d324
unknown <aktarulmondal2740@gmail.com>
2022-07-08 06:18:08 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import Navbar from "./components/Navbar/Navbar";
-import Intro from "./components/Intro/Intro";
-import Services from "./components/Services/Services";
-import "./App.css";
-import Experience from "./components/Experience/Experience";
-import Works from "./components/Works/Works";
-import Portfolio from "./components/Portfolio/Portfolio";
-import Testimonial from "./components/Testimonials/Testimonial";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import { useContext } from "react";
-import { themeContext } from "./Context";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+import EditorPage from './pages/EditorPage';
+
 function App() {
-  const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
-  return (
-    <div
-      className="App"
-      style={{
-        background: darkMode ? "black" : "",
-        color: darkMode ? "white" : "",
-      }}
-    >
-      <Navbar />
-      <Intro />
-      <Services />
-      <Experience />
-      <Works />
-      <Portfolio />
-      <Testimonial />
-      <Contact />
-      <Footer />
-    </div>
-  );
+    return (
+        <>
+            <div>
+                <Toaster
+                    position="top-right"
+                    toastOptions={{
+                        success: {
+                            theme: {
+                                primary: '#4aed88',
+                            },
+                        },
+                    }}
+                ></Toaster>
+            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route
+                        path="/editor/:roomId"
+                        element={<EditorPage />}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
